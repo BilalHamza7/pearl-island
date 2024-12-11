efd8f5410851323edde577460c9df3944957c646
Bilal Hamza <131806480+BilalHamza7@users.noreply.github.com>
2024-12-11 22:20:47 +0530

    implementing sessions

--- src/features/admin/dashboard.js
+++ src/features/admin/dashboard.js
@@ -9,11 +9,19 @@ import axios from "axios";
 export default function Dashboard() {
 
     const navigate = useNavigate();
+    const [verified, setVerified] = useState(false);
 
     const validateSession = async () => {
         try {
-            const response = await axios.get('http://localhost:5000/admin/checkSession', { withCredentials: true });
-            if (!response.data.authenticated) {
+            const response = await axios.get('http://localhost:5000/admin/checkSession', {
+                withCredentials: true,  // Send the session cookie with the request
+            });
+
+            const { authenticated, session } = response.data;
+            console.log('dashboard', authenticated, session);
+
+            setVerified(authenticated);
+            if (!verified) {
                 navigate('/admin');
             }
         } catch (error) {
@@ -24,44 +32,46 @@ export default function Dashboard() {
     useEffect(() => {
         validateSession();
         window.scrollTo(0, 0);
-    }, [])
+    }, []);
 
-    return (
-        <>
-            <Navbar />
-            <div className="flex flex-col items-center w-full min-h-screen py-10">
-                <p className="title_text">Welcome!</p>
-                <p className="subtitle_text mt-2">Get Started Right Away</p>
-                <button onClick={() => navigate('/admin/addNewProduct')} className="button_style mt-6">
-                    Add New Product
-                </button>
-                <div className="flex w-full px-10 mt-10 gap-12">
-                    <ProductsCount />
-                    <div className="flex flex-col w-full gap-5">
-                        <div className="flex flex-col gap-5">
-                            <p className="title_text">Recently Added Products</p>
+    if (verified) {
+        return (
+            <>
+                <Navbar />
+                <div className="flex flex-col items-center w-full min-h-screen py-10">
+                    <p className="title_text">Welcome!</p>
+                    <p className="subtitle_text mt-2">Get Started Right Away</p>
+                    <button onClick={() => navigate('/admin/addNewProduct')} className="button_style mt-6">
+                        Add New Product
+                    </button>
+                    <div className="flex w-full px-10 mt-10 gap-12">
+                        <ProductsCount />
+                        <div className="flex flex-col w-full gap-5">
+                            <div className="flex flex-col gap-5">
+                                <p className="title_text">Recently Added Products</p>
 
-                            {/* Recent Products */}
-                            <RecentProduct />
-                        </div>
+                                {/* Recent Products */}
+                                <RecentProduct />
+                            </div>
 
-                        {/* Recent Requests */}
-                        <div className="flex flex-col gap-5">
-                            <p className="title_text">Recent Price Requests</p>
-                            <RecentRequest />
+                            {/* Recent Requests */}
+                            <div className="flex flex-col gap-5">
+                                <p className="title_text">Recent Price Requests</p>
+                                <RecentRequest />
+                            </div>
                         </div>
                     </div>
+                    <p className="mt-10 title_text">Quick Links</p>
+                    <div className="flex justify-center gap-10 w-full mt-5">
+                        <button onClick={() => navigate('/admin/manageProduct')} className="button_style">
+                            View All Products
+                        </button>
+                        <button onClick={() => navigate('/admin/manageCustomer')} className="button_style">
+                            View All Requests & Inquiries
+                        </button>
+                    </div>
                 </div>
-                <p className="mt-10 title_text">Quick Links</p>
-                <div className="flex justify-center gap-10 w-full mt-5">
-                    <button onClick={() => navigate('/admin/manageProduct')} className="button_style">
-                        View All Products
-                    </button>
-                    <button onClick={() => navigate('/admin/manageCustomer')} className="button_style">
-                        View All Requests & Inquiries
-                    </button>
-                </div>
-            </div>
-        </>
-    )
+            </>
+        )
+    }
 };
